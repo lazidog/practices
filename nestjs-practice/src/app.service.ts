a666e99a0bd3ad7246fbf9ded5769379a91c588d
lazidog <nvtien247@gmail.com>
2023-11-01 02:41:55 +0700

    nestjs-practice: dynamic module

--- nestjs-practice/src/app.service.ts
+++ nestjs-practice/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
+import { HELLO_MESSAGE } from './app.constant';
 
 @Injectable()
 export class AppService {
+  private readonly helloMessage: string;
+
+  constructor(configService: ConfigService) {
+    this.helloMessage = configService.get(HELLO_MESSAGE);
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return this.helloMessage;
   }
 }
